49433065c01dfeef311a23eba0ddab5bb6116a06
appredator <100392528+appredator@users.noreply.github.com>
2022-12-03 15:00:45 -0600

    Condition for when equals is pressed

--- index.js
+++ index.js
@@ -2,17 +2,10 @@
 let firstOperand = 0
 let secondOperand = 0
 let displayValue = 0
-let lastChar = ''
 let symbol = 0
 let symbolValue = 0
 
-//@@ Appending a digit to first operand is not working. Prob not working for second operand either
-
-//@@ needs to be refactored to use firstOperand values for calc and display instead of display value
-// Ive added the display nodes for firstOperand and secondOperand, which have an id of firstDisplay and secondDisplay
-
 //Append Operand method
-// Does logic for determining which value to update work?   !@!!!!!!!
 const buttonGroup = document.getElementById("button-group");
 
 const appendOperand = e => { 
@@ -33,7 +26,6 @@ const appendOperand = e => {
   }
   }
   //conditional to append to first operands current value if value has already been updated
-  // This isnt working. Using too many different variables. Display value needs to be changed to 2 different concurrent vals
   else if (firstOperand != 0){
   displayValue = displayValue + e.target.id
   firstOperand = firstOperand + e.target.id
@@ -98,7 +90,6 @@ const appendOperand = e => {
     console.log("First Operand and symbol were already set. So, second operand has been changed from 0 and set to " + secondOperand)
     }
     }
-    //conditional to append to second operands current value if value has already been updated
     else if (secondOperand != 0){
     displayValue = displayValue + e.target.id
     secondOperand = secondOperand + e.target.id
@@ -132,9 +123,28 @@ const updateSymbol = e => {
     //if symbol and first operand are present and second operand is blank, tell them to enter a value for second operand
   alert("You already have a symbol selected for the operation with your first operand. Please enter a value for the second operand.")
   }
-  else if (firstOperand != 0 && secondOperand !== 0 && symbol !== 0){
-    //if symbol and first and second operands are present, tell them to press equals
-    alert("You already have a symbol selected for the operation with your first and second operand. Please press the equals button if you are ready to calculate.")
+  else if (firstOperand != 0 && secondOperand !== 0 && symbol !== 0 && e.target.id === "-" || e.target.id === "+" || e.target.id === "*" || e.target.id === "*"){
+   //This is the part that catches if all 3 conditions are filled 
+   //and you hit a symbol, it should do the first calculation and update
+   //firstOperand display and secondOperand display and symbol with the
+   //pressed symnbol
+   //
+   //@@@@@@@@@@@@@@@@@@@@
+   //!!!!!!!!!!!!!!!!!
+   /////
+
+   /////
+
+   /////
+    operate()
+    firstOperand = displayValue
+    symbol = e.target.id
+    secondOperand = 0
+    document.querySelector('#firstDisplay').innerText = firstOperand
+    document.querySelector('#secondDisplay').innerText = secondOperand
+    document.querySelector('#display').innerText = displayValue
+    document.querySelector('#symbolDisplay').innerText = e.target.id
+    console.log("You've entered another symbol. First and Second Operand and the symbol were already set. So, results are " + displayValue)
     }
   }
 symbolGroup.addEventListener("click", updateSymbol);
@@ -148,7 +158,6 @@ nextOperand = 0
 lastChar = ''
 symbol = 0
 // console.log("Clear has been called and display Value is " + displayValue)
-let clearedVal = displayValue.toString();
 document.querySelector('#firstDisplay').innerText = 0
 document.querySelector('#secondDisplay').innerText = 0
 document.querySelector('#symbolDisplay').innerText = 0
@@ -192,101 +201,45 @@ const operate = e => {
   
     console.log("Multiply function has been called from operate");
     }
-  
+    firstOperand = 0
+    secondOperand = 0
+    // displayValue = 0
+    symbol = 0
+    symbolValue = 0
+
+    document.querySelector('#firstDisplay').innerText = firstOperand
+    document.querySelector('#secondDisplay').innerText = secondOperand
+    // document.querySelector('#display').innerText = displayValue
+    document.querySelector('#symbolDisplay').innerText = symbol
+
   console.log("The end of the operate function has concluded");
-  // document.querySelector('#display').innerText =  77777
+
 }
 equalsVar.addEventListener("click", operate)
 
-// // make this do event listener changes display first
-// document.querySelector('#equals').addEventListener('click', operate())
-// // non const approach
-// function operates(symbol, firstOperand, secondOperand){
-// console.log("Operate has been called.")
-//   // sym = symbol
-
-//   // a = firstOperand
-
-//   // b = secondOperand
-
-//   if (symbol === "+"){
-
-//   displayValue = add(firstOperand,secondOperand)
-
-//   document.querySelector('#display').innerText =  displayValue 
-
-//   console.log("Add function has been called from operate");
-//   }
-//   else if(symbol === "-"){
-
-//   displayValue =  subtract(firstOperand,secondOperand)
-
-//   document.querySelector('#display').innerText =  displayValue 
-
-//   console.log("Subtraction function has been called from operate");
-//   }
-//   else if(symbol === "/"){
-
-//   displayValue =  divide(firstOperand,secondOperand)
-
-//   document.querySelector('#display').innerText =  displayValue 
-
-//   console.log("Divide function has been called from operate");
-//   }
-//   else if(symbol === "*"){
-
-//   displayValue =  mutliply(firstOperand,secondOperand)
-
-//   document.querySelector('#display').innerText =  displayValue 
-
-//   console.log("Multiply function has been called from operate");
-//   }
-
-// console.log("The end of the operate function has concluded");
-
-// document.querySelector('#display').innerText =  10
-
-// // return console.log(displayValue);
-
-// }
-
 function add(a, b) {
-	return parseInt(a) + parseInt(b);
+let addHolder= parseInt(a) + parseInt(b)
+let add = addHolder.toFixed(2);
+	return add;
 };
 
 function subtract(a, b) {
-	return parseInt(a) - parseInt(b);
+let subHolder = parseInt(a) - parseInt(b)
+let sub = subHolder.toFixed(2)
+	return sub;
 };
 
-
 function mutliply(a, b) {
-return parseInt(a) * parseInt(b);
+let multiHolder= parseInt(a) * parseInt(b)
+let multi = multiHolder.toFixed(2)
+	return multi;
 };
 
 function divide(a, b) {
-	return parseInt(a) / parseInt(b);
+	let divideHolder= parseInt(a) / parseInt(b)
+  let divide = divideHolder.toFixed(2)
+	return divide;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
